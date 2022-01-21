39881ed7f36f6580ce99a437737b937a0fe60109
Артем Остапенко <artemostapenko@MacBook-Air-Artem.local>
2022-01-21 23:57:01 +0300

    Adaptive

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ $(function(){
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img class="slick-prev" src="images/prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img class="slick-next" src="images/next.svg" alt=""></button>',
-        vertical: true
+        vertical: true,
+        responsive: [
+          {
+            breakpoint: 371,
+            settings: {
+              dots: false,
+              arrows: false,
+              autoplay: true,
+              autoplaySpeed: 2000,
+          }
+        }
+      ]
     });
 
     $('.product__name').slick({
@@ -14,6 +25,26 @@ $(function(){
         vertical: true,
         prevArrow: '<button type="button" class="product-prev"><img class="product-prev" src="images/product-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="product-next"><img class="product-next" src="images/product-next.svg" alt=""></button>',
+        responsive: [
+          {
+            breakpoint: 891,
+            settings: {
+              arrows: false,
+              autoplaySpeed: 2000,
+              vertical: false,
+              slidesToShow: 3,
+              dots: true,
+          },
+        breakpoint: 461,
+        settings: {
+        arrows: false,
+        vertical: false,
+        slidesToShow: 1,
+        dots: true
+        }
+      } 
+
+      ]
       });
       $('.product__content').slick({
         slidesToShow: 1,
@@ -24,6 +55,8 @@ $(function(){
         
       });
 
-
+      $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+      });
 
 });
